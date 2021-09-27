b16bd9fb64fe0ca4269fec731c7b4f39226123ef
Alexandher Cordoba Molina <alexandher_1321124129@uptecamac.edu.mx>
2021-09-27 11:38:55 -0500

    CODIGOS DE JavaScript

---
+++ generador.js
@@ -0,0 +1,172 @@
+
+// CODE 1-----------------------------------------------------------------------------------------------------------------
+
+var suma = 0.1;
+var i = 0;
+
+for (var i=0; i<=9; i+0.1){
+   i = parseFloat(i) + parseFloat(suma);
+   console.log(+i);
+}
+
+// CODE 2-------------------------------------------------------------------------------------------------------------------
+
+var c = 0;
+    for (var c=100; c<=199; c++) {
+        console.log(+c);
+    }
+
+// //CODE 3--------------------------------------------------------------------------------------------------------------- 
+
+var suma = 3
+var i = 2;
+
+for(var i=2; i<=20; i+3){
+    i = parseInt(i) + parseInt(suma)
+        console.log(+i);
+}
+
+
+// // CODE 4--------------------------------------------------------------------------------------------------------------
+
+var d = 0;
+var p = parseInt(prompt("Ingrese un numero"))
+var c = parseInt(prompt("confirma tu numero"))
+
+for(var d=p; d<=c; d++){
+        console.log(+c);  
+        console.log(+p*2-1);
+}
+var vb = parseInt(prompt("Ingrese un numero"))
+var ad=((vb*2)+(-1));
+
+for(i=vb; i<ad;i++){
+    console.log(i);
+}
+console.log(vb);
+console.log(ad);
+
+
+// CODE 5-----------------------------------------------------------------------------------------------------------------
+
+
+    var num = parseInt(prompt("Ingrese un numero:"))
+    switch(num){
+        case 1:
+       var num1= parseInt(prompt("Ingrese otro numero:"))
+       var suma= num+num1;
+       alert("La suma es: "+suma)
+        break;
+
+        case 2:
+        var num1= parseInt(prompt("Ingrese otro numero:"))
+        var num2= parseInt(prompt("Ingrese otro numero:"))
+        var suma= num+num1+num2;
+         alert("La suma es: "+suma)
+        break;
+
+        case 3:
+        var num1= parseInt(prompt("Ingrese otro numero:"))
+        var num2= parseInt(prompt("Ingrese otro numero:"))
+        var num3= parseInt(prompt("Ingrese otro numero:"))
+        var suma= num+num1+num2+num3;
+         alert("La suma es: "+suma)
+        break;
+
+        case 4:
+        var num1= parseInt(prompt("Ingrese otro numero:"))
+        var num2= parseInt(prompt("Ingrese otro numero:"))
+        var num3= parseInt(prompt("Ingrese otro numero:"))
+        var num4= parseInt(prompt("Ingrese otro numero:"))
+        var suma= num+num1+num2+num3+num4;
+         alert("La suma es: "+suma)
+        break;
+
+        case 5:
+        var num1= parseInt(prompt("Ingrese otro numero:"))
+        var num2= parseInt(prompt("Ingrese otro numero:"))
+        var num3= parseInt(prompt("Ingrese otro numero:"))
+        var num4= parseInt(prompt("Ingrese otro numero:"))
+        var num5= parseInt(prompt("Ingrese otro numero:"))
+        var suma= num+num1+num2+num3+num4+num5;
+         alert("La suma es: "+suma)
+        break;
+        
+        case 6:
+        var num1= parseInt(prompt("Ingrese otro numero:"))
+        var num2= parseInt(prompt("Ingrese otro numero:"))
+        var num3= parseInt(prompt("Ingrese otro numero:"))
+        var num4= parseInt(prompt("Ingrese otro numero:"))
+        var num5= parseInt(prompt("Ingrese otro numero:"))
+        var num6= parseInt(prompt("Ingrese otro numero:"))
+        var suma= num+num1+num2+num3+num4+num5+num6;
+         alert("La suma es: "+suma)
+        break;
+
+        case 7:
+        var num1= parseInt(prompt("Ingrese otro numero:"))
+        var num2= parseInt(prompt("Ingrese otro numero:"))
+        var num3= parseInt(prompt("Ingrese otro numero:"))
+        var num4= parseInt(prompt("Ingrese otro numero:"))
+        var num5= parseInt(prompt("Ingrese otro numero:"))
+        var num6= parseInt(prompt("Ingrese otro numero:"))
+        var num7= parseInt(prompt("Ingrese otro numero:"))
+        var suma= num+num1+num2+num3+num4+num5+num6+num7;
+         alert("La suma es: "+suma)
+        break;
+
+        case 8:
+        var num1= parseInt(prompt("Ingrese otro numero:"))
+        var num2= parseInt(prompt("Ingrese otro numero:"))
+        var num3= parseInt(prompt("Ingrese otro numero:"))
+        var num4= parseInt(prompt("Ingrese otro numero:"))
+        var num5= parseInt(prompt("Ingrese otro numero:"))
+        var num6= parseInt(prompt("Ingrese otro numero:"))
+        var num7= parseInt(prompt("Ingrese otro numero:"))
+        var num8= parseInt(prompt("Ingrese otro numero:"))
+        var suma= num+num1+num2+num3+num4+num5+num6+num7+num8;
+         alert("La suma es: "+suma)
+        break;
+
+        case 9:
+        var num1= parseInt(prompt("Ingrese otro numero:"))
+        var num2= parseInt(prompt("Ingrese otro numero:"))
+        var num3= parseInt(prompt("Ingrese otro numero:"))
+        var num4= parseInt(prompt("Ingrese otro numero:"))
+        var num5= parseInt(prompt("Ingrese otro numero:"))
+        var num6= parseInt(prompt("Ingrese otro numero:"))
+        var num7= parseInt(prompt("Ingrese otro numero:"))
+        var num8= parseInt(prompt("Ingrese otro numero:"))
+        var num9= parseInt(prompt("Ingrese otro numero:"))
+        var suma= num+num1+num2+num3+num4+num5+num6+num7+num8+num9;
+         alert("La suma es: "+suma)
+        break;
+
+        case 10:
+        var num1= parseInt(prompt("Ingrese otro numero:"))
+        var num2= parseInt(prompt("Ingrese otro numero:"))
+        var num3= parseInt(prompt("Ingrese otro numero:"))
+        var num4= parseInt(prompt("Ingrese otro numero:"))
+        var num5= parseInt(prompt("Ingrese otro numero:"))
+        var num6= parseInt(prompt("Ingrese otro numero:"))
+        var num7= parseInt(prompt("Ingrese otro numero:"))
+        var num8= parseInt(prompt("Ingrese otro numero:"))
+        var num9= parseInt(prompt("Ingrese otro numero:"))
+        var num10= parseInt(prompt("Ingrese otro numero:"))
+        var suma= num+num1+num2+num3+num4+num5+num6+num7+num8+num9+num10;
+         alert("La suma es: "+suma)
+        break;
+
+        default:
+        alert("El número "+num+ " es mayor, por lo tanto, no se puede realizar el proceso");
+    }
+
+// CODE 6
+//------------------------------------------------------------------------------------------------------------------------
+
+var suma = 0;
+
+for (var b=0; b<=100; b++){
+  suma = parseInt(suma) + parseInt(b);
+  console.log(+suma);
+ }
---
+++ op_tortillas.js
@@ -0,0 +1,31 @@
+
+document.write("<h1>OPERACIÓN TORTILAS.<h1>");
+console.log("Proceso para ir por las tortillas.");
+
+var x = " Tortilleria San Juan."
+var y = " Tortilleria la esperanza."
+
+var v = "Abierta."
+var f = "Cerrada."
+
+var precioTortillas = 18
+var dinero = 20 
+
+console.log("Paso 1: Pedir el dinero.");
+
+if(y != v) {
+    console.log("Paso 2: Ir a la" + x)
+}else{
+    console.log("Paso 2: ir a la" + y)
+}
+console.log("Paso 3: Pagar.")
+
+if(precioTortillas > dinero ){
+    console.log("Paso 4: Pedir menos tortillas.")
+}else{
+    console.log("Paso 4: Recibir el cambio.")
+}
+console.log("Paso 5: Regresar a casa.")
+
+
+
